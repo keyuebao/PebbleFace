a0297ad31488392af60219f6d1d8bced8f0c7c27
Keyue Bao <keyuebao@users.noreply.github.com>
2015-05-25 17:10:50 -0700

    Added custom bitmap background

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@ static TextLayer *s_time_layer;
 /* Declare our custom uploaded font from the Resources */
 static GFont s_time_font;
 
+/* Declare two more points, each for bit map and bit map layer */
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
 /* Update the time TextLayer, call it from TickHandler and main_windw_load */
 /* LEARNT: function order matters, as in the one gets called must be declared earlier */
 static void update_time() {
@@ -44,10 +48,17 @@ static void main_window_load (Window *window) {
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   text_layer_set_font(s_time_layer, s_time_font);
   
+  /* Create GBitmap, then set to created BitmapLayer and add it as a child of main Window */
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
   /* Creating TextLayer to display time */
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
   
   /* Improve the layout to be more like a watchface */
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -58,11 +69,15 @@ static void main_window_load (Window *window) {
 }
 
 static void main_window_unload (Window *window) {
-  /* Destroy TextLayer to keep management within loading/unloading of Window */
-  text_layer_destroy(s_time_layer);
+  /* Destroy GBitmap and BitmapLayer */
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
   
   /* Unload GFont safely */
-  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_time_font);  
+  
+  /* Destroy TextLayer to keep management within loading/unloading of Window */
+  text_layer_destroy(s_time_layer);
 } 
 
 /* Helper functions to organize creation/destruction of all Pebble SDK elements */
