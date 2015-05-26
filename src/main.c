abfaeb275b6169ec45e359c8370c066c4bb59754
Keyue Bao <keyuebao@users.noreply.github.com>
2015-05-25 17:19:46 -0700

    Resolved layer conflicts
    For unload, load text/bitmap in proper order. Added update_time in correct location such that time is displayed from the beginning. Everything displays properly now when compile/run with CloudPebble. Learning experience for layer conflicts in Pebble interface.

--- src/main.c
+++ src/main.c
@@ -44,10 +44,6 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 /* Handler functions to manage Window's sub-elements */
 static void main_window_load (Window *window) {
-  /* Create and apply the custom font to TextLayer */
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
-  text_layer_set_font(s_time_layer, s_time_font);
-  
   /* Create GBitmap, then set to created BitmapLayer and add it as a child of main Window */
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
@@ -60,21 +56,25 @@ static void main_window_load (Window *window) {
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   
-  /* Improve the layout to be more like a watchface */
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  /* Create and apply the custom font to TextLayer */
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   /* Add it as a child layer to the Window's root layer */
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  /* Make sure the time is displayed from the beginning */
+  update_time();
 }
 
 static void main_window_unload (Window *window) {
+  /* Unload GFont safely */
+  fonts_unload_custom_font(s_time_font);  
+  
   /* Destroy GBitmap and BitmapLayer */
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
-  
-  /* Unload GFont safely */
-  fonts_unload_custom_font(s_time_font);  
   
   /* Destroy TextLayer to keep management within loading/unloading of Window */
   text_layer_destroy(s_time_layer);
@@ -83,10 +83,7 @@ static void main_window_unload (Window *window) {
 /* Helper functions to organize creation/destruction of all Pebble SDK elements */
 static void init () {
   /* Create main Window element and assign to pointer */
-  s_main_window = window_create();
-  
-  /* Register with TickTimerService */
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);  
+  s_main_window = window_create(); 
   
   /* Set handlers to manage the elements inside the Window */
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -97,8 +94,8 @@ static void init () {
   /* Display window on the watch, second parameter is animated=true */
   window_stack_push(s_main_window, true);
   
-  /* Ensure the time is displayed correctly when watchface loads */
-  update_time();
+  /* Register with TickTimerService */
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); 
 }
 
 static void deinit () {
